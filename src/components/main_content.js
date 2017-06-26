9b17adb850349a2e82abc7eb024f0362d45b7b7b
codey4k <codey4k@gmail.com>
2017-06-26 23:37:28 +0000

    26/06/2017

--- src/components/main_content.js
+++ src/components/main_content.js
@@ -1,9 +1,33 @@
 import React from 'react'
 
 class Main extends React.Component {
+    
+        
+    
     render() {
+        let item = [
+                'item large',
+                'item description',
+                'item red',
+                'item description',
+                'item red',
+                'item description',
+                'item description',
+                'item red',
+                'item large',
+                'item description',
+                'item description',
+                'item red',
+                'item large'
+            ],
+            
+            div = item.map( i => <div className={i} ></div> )
+        
+        
+        
         return(
             <div className="main">
+                {div}
             </div>
         )
     }
