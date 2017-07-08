7fa6cf0c52c6237523346d1053ad161cfc3cc5e8
codey4k <codey4k@gmail.com>
2017-07-08 22:27:10 +0000

    08/07/2017

---
+++ src/components/items.js
@@ -0,0 +1,11 @@
+let item = {
+    'item1': {
+        'img':'#',
+        'class':'item large'
+    }
+}
+
+export default item
+
+// ejemplo
+// https://es.stackoverflow.com/questions/65859/recorrer-objeto-json
--- src/components/services.js
+++ src/components/services.js
@@ -1,22 +1,28 @@
 import React from 'react'
 
-// let Item = (props) =>{
-//     <div className="services__item">
-//         <i>{props.icon}</i>
-//         <p>{props.descriptions}</p>
-//     </div>
-// }
-
-
+let Item = () => 
+    <svg width="100" height="100" viewBox="0 0 400 400" className="svg">
+        <circle cx="200" cy="140" r="50" fill="transparent" stroke="#333" stroke-width="200"/>
+        <path d="M100 280 Q 205 150 300 280" stroke="#333" fill="transparent" stroke-width="200"/>
+    </svg>
 
 class Services extends React.Component {
     render() {
         return(
             <div className="services">
                 <div className="services__container">
-                    <div ></div>
-                    <div ></div>
-                    <div ></div>
+                    <div className="services__item">
+                        <Item />
+                        <h2>Web</h2>
+                    </div>
+                    <div className="services__item">
+                        <Item />
+                        <h2>Hacking</h2>
+                    </div>
+                    <div className="services__item">
+                        <Item />
+                        <h2>Design</h2>
+                    </div>
                 </div>
             </div>
         )
