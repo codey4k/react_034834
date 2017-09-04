4c284c3e0581ef7d8e006318e961b8caa14d13d7
codey4k <codey4k@gmail.com>
2017-09-04 23:34:18 +0000

    edit header component add bubble effect

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -3,9 +3,19 @@ import React from 'react'
 import './header.scss'
 
 class Header extends React.Component {
+    
+    
     render() {
+        let li = [1,2,3,4,5,6,7,8,9,10]
+        
+        
+        let item = li.map( () => <li> </li>)
+        
         return(
             <div className="header">
+                <ul className="bubble">
+                    {item}
+                </ul>
             </div>
         )
     }
